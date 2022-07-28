639340482c6d91c4e8a4e844427aea23f3ffd416
Joep B <joep.barmentlo@echopenfactory.com>
2022-07-28 09:13:05 +0200

    find them script

---
+++ Find_them.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# mv find_me_directories find_me_directories_old 2>/dev/null; true
+cd ~
+rm find_me_directories
+for file in $(find -name ".*.find.me"); do
+	fname=$(basename $file)
+	dname=$(dirname $file)
+	dname=${dname/./$(pwd)}
+	varname=$(echo $fname | cut -d '.' -f 2)
+	echo -e "export $varname=$dname" >> find_me_directories
+	echo -e "Found $varname\t:\t$dname"
+done
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
 # Linux-Ressurect
 Scripts to facilitate the rebuilding of a linux after a fatal mistake. And to save important data easily.
+
+## Data Folder Finder
+
+A simple script to generate an exports file to desired data directories.
+Simply make a `.NAME.find.me` file in any directory you wish.
+The `Find_them.sh` script will find them and generate an exports file.
