103590c00e2f91bac2cf1422ef013cfdfed72b55
Joep B <joep.barmentlo@echopenfactory.com>
2022-08-02 10:24:19 +0200

    search root folder can be set for the find_them script

--- Find_them.sh
+++ Find_them.sh
@@ -1,15 +1,19 @@
 #!/bin/bash
 
-# mv find_me_directories find_me_directories_old 2>/dev/null; true
-cd ~
-rm find_me_directories
+if [[ -z "${FIND_ME_ROOT_DIR}" ]]; then
+  SearchRootDir=$HOME
+else
+  SearchRootDir="${FIND_ME_ROOT_DIR}"
+fi
+cd $SearchRootDir
+rm -f ~/find_me_directories
 for file in $(find -name ".*.find.me"); do
 	fname=$(basename $file)
 	dname=$(dirname $file)
 	dname=${dname/./$(pwd)}
 	varname=$(echo $fname | cut -d '.' -f 2)
 	
-	echo -e "export $varname=$dname" >> find_me_directories
+	echo -e "export $varname=$dname" >> ~/find_me_directories
 	#echo -e "Found $varname\t:\t$dname"
 	printf '%-40s:%s\n' "${varname}" "${dname}"
 done
